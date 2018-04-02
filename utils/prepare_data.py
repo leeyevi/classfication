acc9a72bfbc92f51ffd300b8bd0df263ff22d0fa
leeyevi <leeyevi@gmail.com>
2018-04-02 11:25:22 +0800

    read and write PLY file

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -5,11 +5,17 @@
 # @E-mail  : liyewei20@163.com
 import os
 import numpy as np
+from utils.plyfile import PlyData,PlyElement
+
+#create dataset
 
 label_map = {'01':1,'02':2,'03':3}
 
-def traversal(path):
-    with open('/home/li/PycharmProjects/classfication/data/datas.text', 'w') as f:
+txtfile = '/home/li/PycharmProjects/classfication/data/datas.text'
+
+def traversal(path, txtfile):
+    '''write data paths to a text file with labels(path,label) '''
+    with open(txtfile, 'w') as f:
         for root, dirs, filenames in os.walk(path):
             for filename in filenames:
                 filepath = os.sep.join([root, filename])
@@ -18,6 +24,22 @@ def traversal(path):
                 line = '{},{}\n'.format(filepath, label)
                 f.write(line)
                 #return filepath, dirname,label
+
+#Point cloud IO
+def read_ply(filename):
+    """ read XYZ point cloud from filename PLY file """
+    plydata = PlyData.read(filename)
+    pc = plydata['vertex'].data
+    pc_array = np.array([[x, y, z] for x,y,z in pc])
+    return pc_array
+
+def write_ply(points, filename, text=True):
+    """ input: Nx3, write points to filename as PLY format. """
+    points = [(points[i,0], points[i,1], points[i,2]) for i in range(points.shape[0])]
+    vertex = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'),('z', 'f4')])
+    el = PlyElement.describe(vertex, 'vertex', comments=['vertices'])
+    PlyData([el], text=text).write(filename)
+
 
 if __name__=='__main__':
     datapath = '/home/li/Desktop/3dmodel/train/'
