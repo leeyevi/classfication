ced68b198e65834f5db70e744f9c316636beffd6
leeyevi <leeyevi@gmail.com>
2018-04-02 15:13:55 +0800

    read txt file

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -15,6 +15,14 @@ label_map = {'01':1,'02':2,'03':3}
 
 txtfile = '/home/li/PycharmProjects/classfication/data/datas.text'
 
+def getDataFiles(list_filename):
+    '''read txt return a array(filename,label)'''
+    full = [line.rstrip() for line in open(list_filename)]
+    for i in range(len(full)):
+        full[i] = full[i].split(',')
+    return full
+
+
 def traversal(path, txtfile):
     '''write data paths to a text file with labels(path,label) '''
     with open(txtfile, 'w') as f:
@@ -27,101 +35,24 @@ def traversal(path, txtfile):
                 f.write(line)
                 #return filepath, dirname,label
 
-#Point cloud IO
-def read_ply(filename):
-    """ read XYZ point cloud from filename PLY file """
-    plydata = PlyData.read(filename)
-    pc = plydata['vertex'].data
-    pc_array = np.array([[x, y, z] for x,y,z in pc])
-    return pc_array
-
-def write_ply(points, filename, text=True):
-    """ input: Nx3, write points to filename as PLY format. """
-    points = [(points[i,0], points[i,1], points[i,2]) for i in range(points.shape[0])]
-    vertex = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'),('z', 'f4')])
-    el = PlyElement.describe(vertex, 'vertex', comments=['vertices'])
-    PlyData([el], text=text).write(filename)
-
-
-def draw_point_cloud(input_points, canvasSize=500, space=200, diameter=25,
-                     xrot=0, yrot=0, zrot=0, switch_xyz=[0, 1, 2], normalize=True):
-    """ Render point cloud to image with alpha channel.
-        Input:
-            points: Nx3 numpy array (+y is up direction)
-        Output:
-            gray image as numpy array of size canvasSizexcanvasSize
-    """
-    image = np.zeros((canvasSize, canvasSize))
-    if input_points is None or input_points.shape[0] == 0:
-        return image
-
-    points = input_points[:, switch_xyz]
-    M = euler2mat(zrot, yrot, xrot)
-    points = (np.dot(M, points.transpose())).transpose()
-
-    # Normalize the point cloud
-    # We normalize scale to fit points in a unit sphere
-    if normalize:
-        centroid = np.mean(points, axis=0)
-        points -= centroid
-        furthest_distance = np.max(np.sqrt(np.sum(abs(points) ** 2, axis=-1)))
-        points /= furthest_distance
-
-    # Pre-compute the Gaussian disk
-    radius = (diameter - 1) / 2.0
-    disk = np.zeros((diameter, diameter))
-    for i in range(diameter):
-        for j in range(diameter):
-            if (i - radius) * (i - radius) + (j - radius) * (j - radius) <= radius * radius:
-                disk[i, j] = np.exp((-(i - radius) ** 2 - (j - radius) ** 2) / (radius ** 2))
-    mask = np.argwhere(disk > 0)
-    dx = mask[:, 0]
-    dy = mask[:, 1]
-    dv = disk[disk > 0]
-
-    # Order points by z-buffer
-    zorder = np.argsort(points[:, 2])
-    points = points[zorder, :]
-    points[:, 2] = (points[:, 2] - np.min(points[:, 2])) / (np.max(points[:, 2] - np.min(points[:, 2])))
-    max_depth = np.max(points[:, 2])
-
-    for i in range(points.shape[0]):
-        j = points.shape[0] - i - 1
-        x = points[j, 0]
-        y = points[j, 1]
-        xc = canvasSize / 2 + (x * space)
-        yc = canvasSize / 2 + (y * space)
-        xc = int(np.round(xc))
-        yc = int(np.round(yc))
-
-        px = dx + xc
-        py = dy + yc
-
-        image[px, py] = image[px, py] * 0.7 + dv * (max_depth - points[j, 2]) * 0.3
-
-    image = image / np.max(image)
-    return image
-
-
-def point_cloud_three_views(points):
-    """ input points Nx3 numpy array (+y is up direction).
-        return an numpy array gray image of size 500x1500. """
-    # +y is up direction
-    # xrot is azimuth
-    # yrot is in-plane
-    # zrot is elevation
-    img1 = draw_point_cloud(points, zrot=110 / 180.0 * np.pi, xrot=45 / 180.0 * np.pi, yrot=0 / 180.0 * np.pi)
-    img2 = draw_point_cloud(points, zrot=70 / 180.0 * np.pi, xrot=135 / 180.0 * np.pi, yrot=0 / 180.0 * np.pi)
-    img3 = draw_point_cloud(points, zrot=180.0 / 180.0 * np.pi, xrot=90 / 180.0 * np.pi, yrot=0 / 180.0 * np.pi)
-    image_large = np.concatenate([img1, img2, img3], 1)
-    return image_large
 
 
 if __name__=='__main__':
-    datapath = '/home/li/Desktop/3dmodel/train/'
-    traversal(datapath)
+    #datapath = '/home/li/Desktop/3dmodel/train/'
+    #traversal(datapath)
     # filenames = os.listdir(datapath)
     # for file in filenames:
     #     label = label_map[int(file)]
     #
     #print(dirname)
+    datas = getDataFiles('/home/li/PycharmProjects/classfication/data/datas.text')
+    #narr = np.zeros_like(filenames)
+    # for file in filenames:
+    #     file = file.split(',')
+        #data = narr.append(file)
+
+
+
+    #data = filenames[:,:]
+    #label = filenames[:,1]
+    print(data for data in datas)
